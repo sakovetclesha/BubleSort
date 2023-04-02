b89d8b8c19c1c61c5777dfe0f081e65844a8098c
SAYL <sakovetclesha@gmail.com>
2023-04-02 13:17:08 +0300

    first commit

---
+++ Puzirik1.cpp
@@ -0,0 +1,34 @@
+﻿
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    int array[10];
+    int time = 0; 
+
+    cout << "Hello World!\n";
+
+    for (int i = 0; i < 10; i++)  // Заполнение массива 
+    {
+        cin >> array[i];
+    }
+    
+
+    for (int j = 0; j < 9; j++) // Меняем местами два числа
+    {
+        if (array[j] > array[j + 1])
+        {
+            time = array[j + 1];
+            array[j + 1] = array[j];
+            array[j] = time; 
+        }
+    }
+
+    for (int a = 0; a < 10; a++)  // Заполнение массива 
+    {
+        cout << array[a] << ", ";
+    }
+}
+
